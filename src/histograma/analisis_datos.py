ebdfde65023a2fe6c0914e0d67695f26e83fade9
Ruben A <ruben.arsi@protonmail.ch>
2021-11-11 21:09:27 +0100

    Refactoriza analisis_datos.py y crea una nueva funcion para obtener la poblacion

--- src/histograma/analisis_datos.py
+++ src/histograma/analisis_datos.py
@@ -8,9 +8,19 @@ plt.style.use("dark_background")
 
 
 def histograma(municipio: str, año: str, per_capita: bool, cuantil_inf: int, cuantil_sup: int, tamaño_contenedor: int):
+    """
+    Genera histograma en memoria, no en un fichero
+    """
+    pagos = pagos_basicos(municipio, año, cuantil_inf, cuantil_sup)
+    grafica = genera_grafica(pagos, tamaño_contenedor)
+    return grafica
 
-    # Filtro de datos [pandas] --------------------------------------
 
+def pagos_basicos(municipio: str, año: str, cuantil_inf: int, cuantil_sup: int):
+    """
+    Filtra por los pagos basicos (un tipo de ayuda comun a todos los agricultores)
+    """
+    
     # Lee datos de los PAC de CSV
     pac = pd.read_csv(f"/datos/pac/Beneficiarios_municipio_ejercicio_{año}.txt", header=0, delimiter=";", encoding="cp1252", decimal=",")
 
@@ -26,18 +36,19 @@ def histograma(municipio: str, año: str, per_capita: bool, cuantil_inf: int, cu
     pac = pac[pac.IMPORTE_EUROS < pac.IMPORTE_EUROS.quantile(cuantil_sup)]
 
     # Selecciona el importe de los pagos basicos
-    pagos_basicos = pac["IMPORTE_EUROS"]
+    pagos = pac["IMPORTE_EUROS"]
 
-    # Creación de la gráfica [matplotlib] ---------------------------
+    return pagos
 
+
+def genera_grafica(pagos, tamaño_contenedor):
     # Crea una lista (contenedor) con valores empezando por el cero y
-    # acaba por el maximo de pagos_basicos, en intervalos del tamaño del contenedor.
-    valor_max = max(pagos_basicos)
+    # acaba por el maximo de pagos, en intervalos del tamaño del contenedor.
+    valor_max = max(pagos)
     contenedor = range(0, int(valor_max + valor_max % tamaño_contenedor), tamaño_contenedor)
 
     # Crea titulos para la grafica
-    plt.hist(pagos_basicos, bins=contenedor, color="#FAEBD7")  # This color is called antiquewhite
-    # plt.title(f'Régimen de pago básico\nPAC | {municipio} | {año}')
+    plt.hist(pagos, bins=contenedor, color="#FAEBD7")  # This color is called antiquewhite
     plt.xlabel("€")
     plt.ylabel("Numero de agricultores")
 
@@ -46,6 +57,27 @@ def histograma(municipio: str, año: str, per_capita: bool, cuantil_inf: int, cu
     plt.savefig(buf, format="svg")
     plt.close()
     buf.seek(0)
-    resultado = buf.read()
+    grafica = buf.read()
 
-    return resultado
+    return grafica
+
+
+def numero_habitantes(municipio, año):
+    # Lee datos del CSV
+    poblacion = pd.read_csv(
+        f"/datos/poblacion/poblacion.csv",
+        header=None,
+        delimiter=";",
+        dtype={"municipio": str, "año": int, "poblacion": int},
+        names=["municipio", "año", "poblacion"],
+        engine="python"
+    )
+
+    # Filtra por nombre de municipio
+    poblacion = poblacion[poblacion["municipio"].str.contains(municipio)]
+    
+    # Filtra por el año
+    poblacion = poblacion[poblacion["año"] == año]
+
+    return poblacion["poblacion"].values[0]
+
